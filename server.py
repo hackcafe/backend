f8b611dc108d5dce7a5298c391f1e747b2e11701
Tejas Shah <tejashah88@gmail.com>
2019-07-07 12:39:38 -0700

    added DL routes for backdoor pkgs

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -16,6 +16,18 @@ def fetch_hacks():
 def request_package():
     print(request.json)
     return jsonify({ 'status': 'OK' })
+
+@app.route('/download-windows', methods=['GET'])
+def download_windows():
+    return send_file('download/backdoor.exe', as_attachment=True, attachment_filename='backdoor.exe')
+
+@app.route('/download-mac', methods=['GET'])
+def download_mac():
+    return send_file('download/backdoor.app', as_attachment=True, attachment_filename='backdoor.app')
+
+@app.route('/download-linux', methods=['GET'])
+def download_linux():
+    return send_file('download/backdoor.elf', as_attachment=True, attachment_filename='backdoor.elf')
 
 if __name__ == "__main__":
     app.run(debug=True)
